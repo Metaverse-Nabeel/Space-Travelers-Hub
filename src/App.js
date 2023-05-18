25ee4c4be9697432033b4a26fd893d60e9c9b2b8
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-05-18 17:26:57 +0500

    Display rockets list & give styles to rockets

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Navigation from './components/Navigation';
-import Rockets from './components/Rockets';
 import Missions from './components/Missions';
 import ProfileRoute from './routes/ProfileRoute';
+import RocketsRoute from './routes/RocketsRoute';
 
 const App = () => (
   <>
     <Navigation />
     <Routes>
-      <Route path="/" element={<Rockets />} />
+      <Route path="/" element={<RocketsRoute />} />
       <Route path="/missions" element={<Missions />} />
       <Route path="/profile" element={<ProfileRoute />} />
     </Routes>
-
   </>
 );
 
